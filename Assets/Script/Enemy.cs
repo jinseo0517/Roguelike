e52de5690f5ddba133529fc837ea1b5994811505
jinseo0517 <jinseo0571@gmail.com>
2025-06-08 14:13:33 +0900

    [feat]HUD제작

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -13,19 +13,23 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriter;
+    WaitForFixedUpdate wait;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     void FixedUpdate()
     {
-        if (!isLive) 
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit")) 
             return;
 
         Vector2 dirVec = target.position - rigid.position;
@@ -46,6 +50,10 @@ public class Enemy : MonoBehaviour
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         isLive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
         health = maxHealth;
     }
 
@@ -59,19 +67,34 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet"))
+        if (!collision.CompareTag("Bullet") || !isLive)
             return;
 
         health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
+
 
         if (health > 0)
         {
-            // .. Live, Hit Action
+            anim.SetTrigger("Hit");
         }
         else
         {
-            // .. Die
-            Dead();
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
+        }
+
+        IEnumerator KnockBack()     //코루틴
+        {
+            yield return wait;      //다음 하나의 물리 프레임 딜레이
+            Vector3 playerPos = GameManager.instance.player.transform.position;
+            Vector3 dirVec = transform.position - playerPos;
+            rigid.AddForce(dirVec.normalized * 2, ForceMode2D.Impulse);
         }
 
         void Dead()
