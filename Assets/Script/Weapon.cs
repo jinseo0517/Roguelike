6537c445cd1db6ac0496588be86373cbc7fd4f33
jinseo0517 <jinseo0571@gmail.com>
2025-06-15 20:48:05 +0900

    [feat]장비 업그레이드

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -50,6 +50,8 @@ public class Weapon : MonoBehaviour
 
         if (id == 0)
             Batch();
+
+        player.BroadcastMessage("ApplyGear", SendMessageOptions.DontRequireReceiver);
     }
 
     public void Init(ItemData data)
@@ -83,6 +85,8 @@ public class Weapon : MonoBehaviour
                 speed = 0.3f;
                 break;
         }
+
+        player.BroadcastMessage("ApplyGear",SendMessageOptions.DontRequireReceiver);
     }
 
     void Batch()
