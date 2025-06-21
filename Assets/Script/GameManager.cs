ca76aa57a34a2e447182b5efcbb3e626110ead2a
jinseo0517 <jinseo0571@gmail.com>
2025-06-21 17:21:18 +0900

    [feat]플레이어 선택 기능

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour
     public float gameTime;
     public float maxGameTime = 2 * 10f;
     [Header("# Player Info")]
+    public int playerId;
     public float health;
     public float maxHealth = 100;
     public int level;
@@ -30,10 +31,13 @@ public class GameManager : MonoBehaviour
         instance = this;
     }
 
-    public void GameStart()
+    public void GameStart(int id)
     {
+        playerId = id;
         health = maxHealth;
-        uiLevelUp.Select(0); // 임시 스크립트 (첫번째 캐릭터 선택)
+
+        player.gameObject.SetActive(true);
+        uiLevelUp.Select(playerId % 2); //캐릭터선택
         Resume();
     }
 
