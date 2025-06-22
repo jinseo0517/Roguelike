4e19f6471227db1263a827c9c5cff2d5ffff18e8
jinseo0517 <jinseo0571@gmail.com>
2025-06-22 18:06:05 +0900

    [feat]캐릭터 해금

--- Assets/Script/AchiveManager.cs
+++ Assets/Script/AchiveManager.cs
@@ -1,18 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AchiveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] lockChracter;
+    public GameObject[] unlockChracter;
+
+    enum Achive {  UnlockJaeyong, UnlockSeongeun }
+    Achive[] achives;
+
+    void Awake()
     {
-        
+        achives = (Achive[])Enum.GetValues(typeof(Achive));
+
+        if (!PlayerPrefs.HasKey("MyData"))
+        {
+            Init();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Init()
     {
-        
+        PlayerPrefs.SetInt("MyData", 1);
+
+        foreach (Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 0);
+        }
+    }
+
+    void Start()
+    {
+        UnlockCharacter();
+    }
+
+    void UnlockCharacter()
+    {
+        for (int index = 0; index < lockChracter.Length; index++)
+        {
+            string achiveName = achives[index].ToString();
+            bool isUnlock = PlayerPrefs.GetInt(achiveName) == 1;
+            lockChracter[index].SetActive(!isUnlock);
+            unlockChracter[index].SetActive(isUnlock);
+        }
+    }
+
+    void LateUpdate()
+    {
+        foreach (Achive achive in achives)
+        {
+            CheckAchive(achive);
+        }
+    }
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch (achive)
+        {
+            case Achive.UnlockJaeyong:
+                isAchive = GameManager.instance.kill >= 10;
+                break;
+            case Achive.UnlockSeongeun:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+        }
+
+        if (isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+        }
     }
 }
